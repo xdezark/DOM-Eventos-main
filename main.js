cd0ac714620159cb885ec67dab52adfe0058920f
xdezark <110361952+xdezark@users.noreply.github.com>
2022-09-14 16:43:31 -0300

    Update main.js

--- main.js
+++ main.js
@@ -22,8 +22,6 @@ function cargarPrendas() {
     prendas.push(new Prenda(1, "Buzo", "Negro", "L", 4200, "https://http2.mlstatic.com/D_NQ_NP_979127-MLA45406881553_032021-O.webp"));
     prendas.push(new Prenda(2, "Pantalon", "Negro", 38, 3500, "https://www.tradeinn.com/f/13809/138097991/only---sons-pantalones-leo-linen-mix-gw-3002.jpg"));
     prendas.push(new Prenda(3, "Remera", "Gris", "S", 3000, "https://royalpadel.com.ar/web2/wp-content/uploads/2019/10/REMERA-MADRID-1.jpg"));
-    prendas.push(new Prenda(4, "Campera", "Azul", "M", 13000, "https://www.tiendalibero.com.ar/media/catalog/product/cache/1/image/1200x1200/9df78eab33525d08d6e5fb8d27136e95/a/_/a_63_6.jpg"))
-    prendas.push(new Prenda(5, "Conjunto", "Negro", 38, 11500, "https://images-na.ssl-images-amazon.com/images/I/61hHRlvzgPL._AC_UX522_.jpg"));
 }
 
 cargarPrendas();
@@ -58,14 +56,14 @@ function agregarFuncionAlBoton(){
 }
 
 function agregarAlCarrito(prendas){ 
-   /* console.log(prendas.id); */
-   let existe =carrito.some(pre=>pre.id === prendas.id);
+   
+   let existe =carrito.some(pren=>pren.id === prendas.id);
    if(existe===false){
     prendas.cantidad = 1;
     carrito.push(prendas);
    }
    else{
-     let prendaFind = carrito.find(pre=>pre.id === prendas.id)
+     let prendaFind = carrito.find(pren=>pren.id === prendas.id)
      prendaFind.cantidad++;
    }
    console.log(carrito);
@@ -73,8 +71,9 @@ function agregarAlCarrito(prendas){
 }
 
 function renderizarCarrito(){
+    carritoDiv.innerHTML="";
      carrito.forEach(pren =>{
-        carritoDiv.innerHTML += `<div id="containerCarrito" class="carrito">
+        carritoDiv.innerHTML += `<div id="container" class="containercards">
                            <h3>${pren.tipo}</h3>
                            <p>${pren.color}</p>
                            <p>${pren.talle}</p>
@@ -85,9 +84,29 @@ function renderizarCarrito(){
                          </div>
                          `  
      })
+     borrarProducto()
 }
 
+function borrarProducto(){
+    carrito.forEach(prendas => {
+        document.querySelector(`#btn-borrar${prendas.id}`).addEventListener("click",()=>{
+          let indice = carrito.findIndex(e=>e.id===prendas.id)
+          carrito.splice(indice,1)
+          renderizarCarrito()
+        })
+     });
+}
+
+function total(){
+    let total = document.getElementById("total")
+    let resultado = carrito.reduce((acc,el)=> acc + el.precio, 0)
+    total.innerText = resultado.toFixed(2)
+}
+total();
+
 crearCards();
+
+
 
 
 
